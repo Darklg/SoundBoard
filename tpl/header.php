9fb27f3856b6e47a073869007400a5ec223b9472
Kevin <kevin@rocher-dev.com>
2020-02-08 17:48:50 +0100

    v 0.10.0
    * Quick UX fix on buttons.
    * Set web-app-capable.

--- tpl/header.php
+++ tpl/header.php
@@ -5,6 +5,8 @@
 <title><?php echo PROJECT_NAME; ?></title>
 <link rel="stylesheet" type="text/css" href="assets/css/style.css" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
+<meta name="apple-mobile-web-app-capable" content="yes" />
+<meta name="apple-mobile-web-app-status-bar-style" content="black" />
 </head>
 <body>
 <h1><?php echo PROJECT_NAME; ?></h1>
